ca07cf50c0bf2cca5b995b8831ddcb3020b7aebf
gker_ <gker_inel@hotmail.com>
2022-09-26 14:48:52 +0300

    jenkin proje 3

---
+++ src/test/java/calisma.java
@@ -0,0 +1,54 @@
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class calisma {
+
+@BeforeTest
+public void testStart(){
+    System.out.println("test başlıyor");
+}
+
+@AfterTest
+public  void testsonu(){
+
+    System.out.println("test bitti");
+}
+    @Test
+    public void   testarrays () {
+
+        int[] nums = new int[10];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            nums[i] = (int) (Math.random() * 1000);
+        }
+        System.out.println("nums = " + Arrays.toString(nums));
+        Arrays.sort(nums);
+
+
+        int max1 = nums[nums.length - 1];
+        int max2 = nums[nums.length - 2];
+
+        System.out.println("Maximu = " + max1);
+        System.out.println("En büyük ikinci= " + max2);
+
+
+            Assert.assertTrue(max1 >= max2);
+
+        for (int i = 0; i < nums.length-1; i++) {
+            Assert.assertTrue(max1 >= nums[i]);
+        }
+        for (int i = 0; i < nums.length-2; i++) {
+            Assert.assertTrue(max2 >= nums[i]);
+        }
+
+        }
+
+    }
+
+
+
