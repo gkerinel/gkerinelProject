71dc96830c802f7d497f29c37384e5cc6b59fade
gker_ <gker_inel@hotmail.com>
2022-10-03 22:27:02 +0300

    jenkins

--- src/test/java/unitTest.java
+++ src/test/java/unitTest.java
@@ -15,10 +15,11 @@ public class unitTest {
     @Test
     public void validation() {
 
-        int[] num = {4, 5, 12, 33, 67, 23, 45, 2345, 23, 678};
+        int[] num = {4, 5, 12, 33, 67, 23, 45, 2345, 17, 678};
 
       int[] result= unitTest.maxValues(num);
-        System.out.println(result[0]);
+        System.out.println("max1 value= "+result[0]);
+        System.out.println("max2 value= "+result[1]);
 
       Assert.assertEquals(2345, result[0]);
       Assert.assertEquals(678,result[1]);
